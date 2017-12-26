8c8c2885a7d7ac44d56b45cdbe4f2ae2ef42ae99
Manas <prometheansacrifice@gmail.com>
2017-12-26 17:38:57 +0530

    WIP2

--- scripts/jest/config.react-dom-test-suite.js
+++ scripts/jest/config.react-dom-test-suite.js
@@ -1,15 +1,32 @@
-const packageJSON = require('../../package.json');
-module.exports = Object.assign(
-  {},
-  // packageJSON.jest,
-  {
-    setupFiles: ['./test/setup.js'],
-    moduleNameMapper: {
-      'react-dom': '<rootDir>/src/index.js',
-    },
-    testRegex:
-      'test/react-test-suite/packages/react-dom/src/__tests__/.*\\.js$',
-    rootDir: process.cwd(),
-    roots: ['<rootDir>/test/react-test-suite/packages'],
+// const packageJSON = require('../../package.json');
+// module.exports = Object.assign(
+//   {},
+//   // packageJSON.jest,
+//   {
+//     transform:{
+//       '.*': require.resolve('./preprocess.react-test-suite.js')
+//     },
+//     setupFiles: ['./test/setup.js'],
+//     moduleNameMapper: {
+//       'react-dom': '<rootDir>/lib/index.js',
+//     },
+//     testRegex:
+//       'test/react-test-suite/packages/react-dom/src/__tests__/.*\\.js$',
+//     rootDir: process.cwd(),
+//     roots: ['<rootDir>/test/react-test-suite/packages'],
+//   },
+// );
+
+const reactJestSourceConfig = require('../../test/react-test-suite/scripts/jest/config.source.js');
+
+module.exports = Object.assign({}, reactJestSourceConfig, {
+  modulePathIgnorePatterns: [
+    '<rootDir>/test/react-test-suite/scripts/rollup/shims/',
+    '<rootDir>/test/react-test-suite/scripts/bench/',
+  ],
+  moduleNameMapper: {
+    'react-dom': '<rootDir>/lib/index.js',
   },
-);
+  testRegex: '/react-dom/src/__tests__/[^/]*(\\.js)$',
+  roots: ['<rootDir>/test/react-test-suite/packages'],
+});
