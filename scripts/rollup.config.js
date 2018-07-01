7f8202f4b184d91135df03efac37b4dbc0fb344b
Manas <prometheansacrifice@gmail.com>
2018-07-01 18:29:12 +0530

    Back to using gcc js and simple mode

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,37 +1,6 @@
 const babel = require('rollup-plugin-babel');
 const commonjs = require('rollup-plugin-commonjs');
-const tmp = require('tmp');
-const ClosureCompiler = require('google-closure-compiler').compiler;
-const { writeFileSync } = require('fs');
-// const closure = require('rollup-plugin-closure-compiler-js');
-function compile(flags) {
-  return new Promise((resolve, reject) => {
-    const closureCompiler = new ClosureCompiler(flags);
-    closureCompiler.run(function(exitCode, stdOut, stdErr) {
-      // if (!stdErr) {
-      //   resolve(stdOut);
-      // } else {
-      //   reject(new Error(stdErr));
-      // }
-      resolve(stdOut);
-    });
-  });
-}
-function closure(flags = {}) {
-  return {
-    name: 'closure-plugin',
-    async transformBundle(code) {
-      const inputFile = tmp.fileSync();
-      const tempPath = inputFile.name;
-      flags = Object.assign({}, flags, { js: tempPath });
-      writeFileSync(tempPath, code, 'utf8');
-      return compile(flags).then(compiledCode => {
-        inputFile.removeCallback();
-        return { code: compiledCode };
-      });
-    },
-  };
-}
+const closure = require('rollup-plugin-closure-compiler-js');
 
 const replace = require('rollup-plugin-replace');
 const resolve = require('rollup-plugin-node-resolve');
@@ -48,23 +17,21 @@ module.exports = {
   plugins: [
     replace({
       __DEV__: dev,
-      __SVG__: true,
+      __SVG__: true, // Needs attention! A way/config to selectively turn this on or off
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }), // Needs attention! A way/config to selectively turn this on or off
+    }),
     babel(),
     resolve({ browser: true }), // browser: true for warning module
     commonjs(),
     !dev &&
       closure({
-        // compilationLevel: 'SIMPLE',
-        compilationLevel: 'ADVANCED',
+        compilationLevel: 'SIMPLE',
         languageIn: 'ECMASCRIPT5_STRICT',
         languageOut: 'ECMASCRIPT5_STRICT',
-        // env: 'CUSTOM',
+        env: 'CUSTOM',
         rewritePolyfills: false,
         applyInputSourceMaps: false,
         processCommonJsModules: false,
-        jscompOff: 'checkVars',
       }),
     logBundleSize(),
   ].filter(Boolean),
